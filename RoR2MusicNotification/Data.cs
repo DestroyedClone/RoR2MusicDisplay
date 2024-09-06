36f60a5fbe83c7fa31c7f5763b3aa09e5b370a8c
DestroyedClone <72328339+DestroyedClone@users.noreply.github.com>
2024-09-06 01:18:05 -0700

    Moved track names to Data class

---
+++ RoR2MusicNotification/Data.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RoR2MusicNotification
+{
+    internal class Data
+    {
+
+        public static Dictionary<string, string> trackNames = new Dictionary<string, string>()
+        {
+            {"muBossfightDLC1_10", "Having Fallen, It Was Blood"},
+            {"muBossfightDLC1_12", "A Boat Made from a Sheet of Newspaper"},
+            {"muEscape", "Escape (Unnamed Track)"},
+            {"muFULLSong02", "Into the Doldrums"},
+            {"muFULLSong06", "Nocturnal Emission"},
+            {"muFULLSong07", "Evapotranspiration"},
+            {"muFULLSong18", "Disdrometer"},
+            {"muFULLSong19", "Terra Pluviam"},
+            {"muGameplayBase_09", "They Might As Well Be Dead"},
+            {"muGameplayDLC1_01", "Once in a Lullaby"},
+            {"muGameplayDLC1_03", "Out of Whose Womb Came the Ice?"},
+            {"muGameplayDLC1_06", "Who Can Fathom the Soundless Depths?"},
+            {"muGameplayDLC1_08", "A Placid Island of Ignorance"},
+            {"muGameplayDLC2_01", "It Can't Rain All the Time"},
+            {"muGameplayDLC2_02", "A Tempestuous Noise of Thunder and Lightning Heard"},
+            {"muIntroCutscene", "Through a Cloud, Darkly"},
+            {"muLogbook", "The Dehydration of Risk of Rain 2"},
+            {"muMainEndingFull", "Lacrimosum"},
+            {"muMainEndingOutroA", "Lacrimosum"},
+            {"muMainEndingOutroB", "Lacrimosum"},
+            {"muMenu", "Risk of Rain 2"},
+            {"muMenuDLC1", "Prelude in D flat major"},
+            {"muNone", ""},
+            {"muRaidfightDLC1_07", "The Face of the Deep"},
+            {"muSong04", "Parjanya"},
+            {"muSong05", "Thermodynamic Equilibrium"},
+            {"muSong08", "A Glacier Eventually Farts (And Don't You Listen to the Song of Life)"},
+            {"muSong13", "The Raindrop that Fell to the Sky"},
+            {"muSong14", "The Rain Formerly Known as Purple"},
+            {"muSong16", "Hydrophobia"},
+            {"muSong21", "Petrichor V"},
+            {"muSong22", "Köppen As Fuck"},
+            {"muSong23", "Antarctic Oscillation"},
+            {"muSong24", "...con lentitud poderosa"},
+            {"muSong25", "You're Gonna Need A Bigger Ukulele"},
+        };
+    }
+}
--- RoR2MusicNotification/Plugin.cs
+++ RoR2MusicNotification/Plugin.cs
@@ -22,45 +22,6 @@ namespace RoR2MusicNotification
         public static HGTextMeshProUGUI notifTextMesh;
         public static GameObject ReferenceGameObject;
 
-        public static Dictionary<string, string> trackNames = new Dictionary<string, string>()
-        {
-            {"muBossfightDLC1_10", "Having Fallen, It Was Blood"},
-            {"muBossfightDLC1_12", "A Boat Made from a Sheet of Newspaper"},
-            {"muEscape", "Escape (Unnamed Track)"},
-            {"muFULLSong02", "Into the Doldrums"},
-            {"muFULLSong06", "Nocturnal Emission"},
-            {"muFULLSong07", "Evapotranspiration"},
-            {"muFULLSong18", "Disdrometer"},
-            {"muFULLSong19", "Terra Pluviam"},
-            {"muGameplayBase_09", "They Might As Well Be Dead"},
-            {"muGameplayDLC1_01", "Once in a Lullaby"},
-            {"muGameplayDLC1_03", "Out of Whose Womb Came the Ice?"},
-            {"muGameplayDLC1_06", "Who Can Fathom the Soundless Depths?"},
-            {"muGameplayDLC1_08", "A Placid Island of Ignorance"},
-            {"muGameplayDLC2_01", "It Can't Rain All the Time"},
-            {"muGameplayDLC2_02", "A Tempestuous Noise of Thunder and Lightning Heard"},
-            {"muIntroCutscene", "Through a Cloud, Darkly"},
-            {"muLogbook", "The Dehydration of Risk of Rain 2"},
-            {"muMainEndingFull", "Lacrimosum"},
-            {"muMainEndingOutroA", "Lacrimosum"},
-            {"muMainEndingOutroB", "Lacrimosum"},
-            {"muMenu", "Risk of Rain 2"},
-            {"muMenuDLC1", "Prelude in D flat major"},
-            {"muNone", ""},
-            {"muRaidfightDLC1_07", "The Face of the Deep"},
-            {"muSong04", "Parjanya"},
-            {"muSong05", "Thermodynamic Equilibrium"},
-            {"muSong08", "A Glacier Eventually Farts (And Don't You Listen to the Song of Life)"},
-            {"muSong13", "The Raindrop that Fell to the Sky"},
-            {"muSong14", "The Rain Formerly Known as Purple"},
-            {"muSong16", "Hydrophobia"},
-            {"muSong21", "Petrichor V"},
-            {"muSong22", "Köppen As Fuck"},
-            {"muSong23", "Antarctic Oscillation"},
-            {"muSong24", "...con lentitud poderosa"},
-            {"muSong25", "You're Gonna Need A Bigger Ukulele"},
-        };
-
         public void Awake()
         {
             RoR2MusicNotification.ModConfig.Initialize(Config);
@@ -110,7 +71,7 @@ namespace RoR2MusicNotification
 
         public static string GetTrackInfo(string trackDefName)
         {
-            if (trackNames.TryGetValue(trackDefName, out var trackName))
+            if (Data.trackNames.TryGetValue(trackDefName, out var trackName))
             {
                 if (trackName == "muNone")
                     return string.Empty;
